8bb2ee2f33ec53dab4284712baee73803e28d7a0
Viktor <viktor23112004>
2024-06-05 16:34:33 +0300

    video_31_05_2024_01_14

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
   imports: [
-    ProductComponent
+    ProductComponent,
+    NgFor
   ],
+  providers: [ProductService],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+
+  products: Product[] = []
+
+  constructor (private productServise: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.productServise.getProducts()
+    console.log(this.products)
+    
+  }
 
 }
